7561cd66d9abde52db2131d6d7b6ee4327b78c71
D-A-Borisov <denisborisov1000@gmail.com>
2024-11-29 23:35:15 +0500

    Initial commit

---
+++ src/lr1/Example10.java
@@ -0,0 +1,18 @@
+package lr1;
+
+import java.util.Scanner;
+
+public class Example10 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter the year of birth");
+        int birthYear = in.nextInt();
+
+        int currentYear = 2024;
+
+        int age = currentYear - birthYear;
+
+        System.out.println("Your age: " + age);
+    }
+}
---
+++ src/lr1/Example12.java
@@ -0,0 +1,18 @@
+package lr1;
+
+import java.util.Scanner;
+
+public class Example12 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+
+        System.out.print("Enter your age: ");
+        int age = in.nextInt();
+
+        int currentYear = 2024;
+
+        int years = currentYear - age;
+
+        System.out.println("Your year: " + years);
+    }
+}
